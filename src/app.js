a6ada1c69252c3e3e5cd98f6dcb24d3055de6195
prapakaran-developer <prapakaran.s@creatah.com>
2026-04-02 10:15:43 +0530

    removed the frontend url

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const settingsRoutes = require('./routes/settings');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
